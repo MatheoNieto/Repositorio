2c4e90d7ae3bda6b7f2e8f12448d1a929eb5444e
JuanPa7575 <juanpa290614@gmail.com>
2019-09-18 12:48:26 -0500

    Problemas con PDF

--- application/views/contenidop/contenedorCSSScripts.php
+++ application/views/contenidop/contenedorCSSScripts.php
@@ -2,7 +2,7 @@
 <!--------------------------------------------------CSS----------------------------------------------------------------------->
 <!---------------------------------------------------------------------------------------------------------------------------->
         
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/jquery.mobile.custom.structure.css"/>
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/jquery.mobile.custom.structure.css"/>
         <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/jquery.mobile.custom.theme.css"/>
         <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/formeter.css"/> 
 
@@ -11,6 +11,7 @@
 <!------------------------------------------------Scripts--------------------------------------------------------------------->
 <!---------------------------------------------------------------------------------------------------------------------------->
         
-        <script src="<?php echo base_url(); ?>scripts/jquery-1.11.3.min.js" ></script>
+        <script src="<?php echo base_url(); ?>scripts/jquery-1.11.3.min.js" ></script>     
         <script src="<?php echo base_url(); ?>scripts/jquery.mobile.custom.js" ></script>
         <script src="<?php echo base_url(); ?>scripts/formeter.js" ></script> 
+        <script src="<?php echo base_url(); ?>scripts/jquery.validate.js " ></script>
