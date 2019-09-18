a16381b4be9d5e839361ac022cda93d30e42d04d
JuanPa7575 <juanpa290614@gmail.com>
2019-09-18 10:19:23 -0500

    Validate solucionado

--- application/views/contenidop/contenedorCSSScripts.php
+++ application/views/contenidop/contenedorCSSScripts.php
@@ -11,6 +11,7 @@
 <!------------------------------------------------Scripts--------------------------------------------------------------------->
 <!---------------------------------------------------------------------------------------------------------------------------->
         
-        <script src="<?php echo base_url(); ?>scripts/jquery-1.11.3.min.js" ></script>
+        <script src="<?php echo base_url(); ?>scripts/jquery-1.11.3.min.js" ></script>     
         <script src="<?php echo base_url(); ?>scripts/jquery.mobile.custom.js" ></script>
         <script src="<?php echo base_url(); ?>scripts/formeter.js" ></script> 
+        <script src="<?php echo base_url(); ?>scripts/jquery.validate.js " ></script>   
