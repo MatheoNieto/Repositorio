161e37f94a3a4fa21f8f972205c022f17da0fab1
JuanPa7575 <juanpa290614@gmail.com>
2019-09-18 13:01:05 -0500

    Segundo intento PDF

--- application/views/contenidop/contenedorCSSScripts.php
+++ application/views/contenidop/contenedorCSSScripts.php
@@ -14,4 +14,3 @@
         <script src="<?php echo base_url(); ?>scripts/jquery-1.11.3.min.js" ></script>     
         <script src="<?php echo base_url(); ?>scripts/jquery.mobile.custom.js" ></script>
         <script src="<?php echo base_url(); ?>scripts/formeter.js" ></script> 
-        <script src="<?php echo base_url(); ?>scripts/jquery.validate.js " ></script>
